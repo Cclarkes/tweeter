93f5b134772c601de1cca07d9e27ca90bf12af6a
CClarkes <connorclarkes@gmail.com>
2019-02-23 18:52:33 +0000

    linting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,23 +7,31 @@ $(document).ready(function() {
   function createTweetElement(data) {
 
     let $tweet = $("<article>").addClass("tweet");
+
   // Above the article is declared, while below we have the header declarations and appends made.
+
     let $header = $("<header>").addClass("Avatar");
     let $image= $("<img>").addClass("imgAvatar").attr("src", data.user.avatars.small);
     let $h3 = $("<h3>").text(data.user.name);
     let $p = $("<p>").addClass("userTag").text(data.user.handle);
       $header.append($image).append($h3).append($p);
       $tweet.append($header);
+
   // Below here is all the code for body declaration and appendments.
+
     let $body = $("<div>").addClass("tweetContent");
     let $p2 = $("<p>").text(data.content.text);
         $body.append($p2);
+
   // And finally, the footer is shown below: declarations and also the final amendment + return
-    let $myIcons = `<span>
+
+    let $myIcons = 
+      `<span>
         <i class="fas fa-flag-checkered" alt="flag"></i>
         <i class="fas fa-recycle" alt="retweet"></i>
         <i class="far fa-thumbs-up" alt="like"></i>
       </span>`
+
     let $footer = $("<footer>").addClass("postDate");
         $footer.text(data.created_at);
         $footer.append($myIcons);
@@ -56,17 +64,17 @@ $(document).ready(function() {
     if($input.val().length > 140) {
       $("#error2").show(200);
     } else {
-    $.ajax("/tweets", {
-      method: "POST",
-      data: $input.serialize(),
-      statusCode: {
-        400: function() {
-          $("#error1").show(200); 
+      $.ajax("/tweets", {
+        method: "POST",
+        data: $input.serialize(),
+        statusCode: {
+          400: function() {
+            $("#error1").show(200); 
+          }
         }
-      }
-    })
-    $(".counter").html(140),
-    $(".newTweet").val(``);
+      })
+      $(".counter").html(140),
+      $(".newTweet").val(``);
     }
   loadTweets();
   })
